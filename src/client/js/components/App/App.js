2a088d1bbe30538c58a4e880ab3885f7af75f3dc
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-03-21 23:34:28 +0000

    feat: added footer

--- src/client/js/components/App/App.js
+++ src/client/js/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Header } from './../presentational/Header/Header';
+import { Footer } from './../presentational/Footer/Footer';
 import Styles from './Styles.css';
 
 export const App = () => (
   <div>
     <Header />
+    <Footer />
   </div>
 );
