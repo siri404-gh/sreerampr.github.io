66dcaef8bb8f7e60cce81c33898d99d4fb713f46
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-03-22 00:23:13 +0000

    feat: added google login

--- src/client/js/components/App/App.js
+++ src/client/js/components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Header } from './../presentational/Header/Header';
 import { Footer } from './../presentational/Footer/Footer';
+import Login from './../presentational/Login/Login';
 import Styles from './Styles.css';
 
 export const App = () => (
   <div>
     <Header />
+    <Login />
     <Footer />
   </div>
 );
