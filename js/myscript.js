5bb3913cebe4c8c5e93d8e359245b14991690c45
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-05-19 08:07:54 +0100

    updates

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,10 @@
 window.onload = function() {
     var links = document.getElementsByClassName('link');
-    
+
     for (var i = 0; i< links.length; i++) {
         links[i].addEventListener('click', function() {
             var id = this.innerHTML.toLowerCase();
-            var top = $("#"+id).offset().top - 50;
+            var top = $("#"+id).offset().top;
             $('html, body').animate({
                 scrollTop: top
             }, 500);
