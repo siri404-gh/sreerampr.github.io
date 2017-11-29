41dcdd5c6fe652c03ecbcaf49b740b5cb2107c46
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-11-29 14:00:00 +0100

    added og meta types

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,23 @@
-window.onload = function() {
+window.onload = function () {
     var links = document.getElementsByClassName('link');
 
-    for (var i = 0; i< links.length; i++) {
-        links[i].addEventListener('click', function() {
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function () {
             var id = this.innerHTML.toLowerCase().replace(' ', '');
-            var top = $("#"+id).offset().top;
-            $('html, body').animate({
-                scrollTop: top
-            }, 500);
+            const val = offset(document.getElementById(id));
+            console.log(val);
+            window.scroll({
+                top: val.top,
+                left: 0,
+                behavior: 'smooth'
+            });
         });
     }
 };
+
+function offset(el) {
+    var rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
